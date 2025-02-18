f7e7ce8136b1d70f81c5879a0aeea36644992832
Lukas Martínez <lukasmartinezc@gmail.com>
2025-02-18 10:45:10 -0300

    exercise 11 and 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,46 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((acc, person) => {
+    const currentYear = new Date().getFullYear()
+    let age = (person.yearOfDeath || currentYear) - person.yearOfBirth
+    let oldestAge = (acc.yearOfDeath || currentYear) - acc.yearOfBirth
 
+    return age > oldestAge ? person : acc
+  })
+
+  /* const personsAndYears =  people.reduce((acc, person) => {
+    const currenteYear = new Date().getFullYear()
+    let yearsAlive = !person.yearOfDeath ? (currenteYear - person.yearOfBirth) : (person.yearOfDeath - person.yearOfBirth)
+      personUpdate = {
+        name: person.name,
+        yearsAlive
+      }
+      acc.push(personUpdate)
+      return acc
+    }, [])
+
+  const personMoreOld = personsAndYears.reduce((acc, person) => {
+    return person.yearsAlive > acc.yearsAlive ? person : acc
+  }, personsAndYears[0]) 
+
+  return personMoreOld */
 };
+
+console.log(findTheOldest([
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]))
 
 // Do not edit below this line
 module.exports = findTheOldest;
